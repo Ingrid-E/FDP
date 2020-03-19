01789e6fe97ad7f6dc7ce44b2664523be7adfe71
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2020-03-19 11:11:04 -0500

    codes

---
+++ Codes/mayorL.js
@@ -0,0 +1,32 @@
+//Ingrid Echeverri ingrid.echeverri@correounivalle.edu.co
+
+const {cons, first, rest, isEmpty, isList, length} = require('functional-light');
+
+
+/**
+ * Buscar el numero mayor en una lista
+ * @param {Array} list
+ * @returns {*}
+ * @example mayorL([1,2,3]) // => 3
+ */
+
+function mayorL(list){
+
+if (length(list)==1){
+  return first(list)
+}
+
+{if (isEmpty(list)) {
+   return null
+    } else {
+      return Math.max(first(list),mayorL(rest(list)))
+    }
+ }
+}
+
+
+
+console.log(mayorL([]))
+console.log(mayorL([1,6,2]))
+console.log(mayorL([-9]))
+console.log(mayorL([1,6,20,3,-6, 8, 9]))
