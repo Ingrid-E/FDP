8fc22818ca6051df7d4df639c95dde5c803cbe00
Ingrid <ingrid.echeverri@correounivalle.edu.co>
2020-03-21 21:43:54 -0400

    fibonacci

--- Codes/fibonacciList.js
+++ Codes/fibonacciList.js
@@ -1,10 +1,41 @@
 const {cons, first, rest, isEmpty, isList, length} = require('functional-light');
 
 function fibolist(n){
-    if(n<=0)return 1;
-    return (n-1)+fibolist(n-2)
+    if(n<2)return n;
+
+    return fibolist(n-1)+fibolist(n-2)
 }
 
-console.log(fibolist(3));
-console.log(fibolist(2));
-console.log(fibolist(1));
+function fibolist2(n){
+    if(n==1) return 1;
+    return cons(fibolist(n-1),[fibolist(n)])
+    
+}
+
+console.log(fibolist2(4));
+console.log(fibolist2(2));
+console.log(fibolist2(8));
+
+/*
+(4)
+(4-1) + (4-2)
+(3)
+(3-1) + (3-2)return 1
+(2)
+(2-1) + (2-2)return 0
+(1)return 1
+
+(4-2)
+(2)
+(2-1) + (2-2) return 0 
+(1) return 1 
+
+1+1 +1 
+
+
+
+    
+
+
+
+*/
